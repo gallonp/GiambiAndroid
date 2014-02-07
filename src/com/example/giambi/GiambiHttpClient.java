d7562fba3dd9d26216a1eea26829e3a5c3e58c5c
Haoli Du <henry54809@gmail.com>
2014-02-07 13:24:40 -0500

    a

--- src/com/example/giambi/GiambiHttpClient.java
+++ src/com/example/giambi/GiambiHttpClient.java
@@ -15,52 +15,53 @@ import android.util.Log;
 
 public class GiambiHttpClient {
 
-	public static HttpClient httpClient = new DefaultHttpClient();  
-	
-	public static class AsynReader extends AsyncTask<HttpRequest, Void, HttpResponse> {
+    public static HttpClient httpClient = new DefaultHttpClient();
 
-		@Override
-		protected HttpResponse doInBackground(HttpRequest... requests) {
-			HttpRequest req = requests[0];
-			HttpResponse resp;
-			try {
-				resp = httpClient.execute((HttpUriRequest) req);
-				Log.v("asynReader","asynTaskComplete");
-			} catch (ClientProtocolException e) {
-				// TODO Auto-generated catch block
-				Log.e(this.toString(),e.toString());
-				resp = null;
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				Log.e(this.toString(),e.toString());
-				resp = null;
-			}
-			return resp;
-		}
-	}
-	
-	public static HttpResponse getResponse(HttpRequest request){
-		AsynReader asynReader = new AsynReader();
-		asynReader.execute(request);
-		HttpResponse resp;
-		Log.v("getResponse","asynTaskComplete");
-		try {
-			resp = asynReader.get();
-			Log.v("getResponse","response got");
-			return resp;
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			Log.e("getResponse",e.toString());
-			return null;
-		} catch (ExecutionException e) {
-			// TODO Auto-generated catch block
-			Log.e("getResponse",e.toString());
-			return null;
-		} catch (IllegalStateException e) {
-			// TODO Auto-generated catch block
-			Log.e("getResponse",e.toString());
-			return null;
-		} 
-		
-	}
+    public static class AsynReader extends
+            AsyncTask<HttpRequest, Void, HttpResponse> {
+
+        @Override
+        protected HttpResponse doInBackground(HttpRequest... requests) {
+            HttpRequest req = requests[0];
+            HttpResponse resp;
+            try {
+                resp = httpClient.execute((HttpUriRequest) req);
+                Log.v("asynReader", "asynTaskComplete");
+            } catch (ClientProtocolException e) {
+                // TODO Auto-generated catch block
+                Log.e(this.toString(), e.toString());
+                resp = null;
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                Log.e(this.toString(), e.toString());
+                resp = null;
+            }
+            return resp;
+        }
+    }
+
+    public static HttpResponse getResponse(HttpRequest request) {
+        AsynReader asynReader = new AsynReader();
+        asynReader.execute(request);
+        HttpResponse resp;
+        Log.v("getResponse", "asynTaskComplete");
+        try {
+            resp = asynReader.get();
+            Log.v("getResponse", "response got");
+            return resp;
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            Log.e("getResponse", e.toString());
+            return null;
+        } catch (ExecutionException e) {
+            // TODO Auto-generated catch block
+            Log.e("getResponse", e.toString());
+            return null;
+        } catch (IllegalStateException e) {
+            // TODO Auto-generated catch block
+            Log.e("getResponse", e.toString());
+            return null;
+        }
+
+    }
 }
