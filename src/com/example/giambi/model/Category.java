3d584a69bc0240a106e32695f9c7e706ca58cfe4
gallonp <gallonpig@gmail.com>
2014-03-01 21:53:12 -0500

    Updated Transaction.java and Category.java

---
+++ src/com/example/giambi/model/Category.java
@@ -0,0 +1,20 @@
+package com.example.giambi.model;
+
+public class Category {
+
+	public static final String uncategorized = "Uncategorized";
+	public static final String rentalCarTaxi = "Rental Car & Taxi";
+	public static final String groceries = "Groceries";
+	public static final String shopping = "shopping";
+	public static final String clothing = "Clothing";
+	public static final String creditCardPayment = "Credit Card Payment";
+	public static final String fastfood = "public static final String";
+	public static final String cashATM = "Cash & ATM";
+	public static final String pharmacy = "Pharmacy";
+	public static final String restaurants = "Restaurants";
+	public static final String bookSupplies = "Book and Supplies";
+	public static final String paycheck = "PayCheck";
+	public static final String mobilePhone = "Mobile Phone";
+	public static final String coffeeShop = "Coffee Shop";
+
+}
--- src/com/example/giambi/model/Transaction.java
+++ src/com/example/giambi/model/Transaction.java
@@ -2,8 +2,18 @@ package com.example.giambi.model;
 
 public class Transaction {
 
-	public Transaction() {
-		// TODO Auto-generated constructor stub
+	public String name;
+	public double amount;
+	public String category;
+	
+	public Transaction(String name, double amount, String category) {
+		this.name = name;
+		this.amount = amount;
+		this.category = category;
 	}
+	
+	
+	
+	
 
 }
