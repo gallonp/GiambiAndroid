7c22f6a484ded5203f300a848770241c1d77405f
gallonp <gallonpig@gmail.com>
2014-03-27 18:09:32 -0400

    add mock library

---
+++ src/com/example/giambi/test/TransactionTest.java
@@ -0,0 +1,18 @@
+package com.example.giambi.test;
+
+import org.mockito.Mock;
+
+import com.example.giambi.GiambiHttpClient;
+
+import junit.framework.TestCase;
+
+public class TransactionTest extends TestCase {
+	
+@Mock
+private GiambiHttpClient giambiHttpClient;
+	
+
+
+	
+	
+}
